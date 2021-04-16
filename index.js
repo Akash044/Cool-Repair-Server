d66b215d4364683f15f92276ec6605aa0c976088
Md. Akash Hossain <akash.cse.ruet@gmail.com>
2021-04-16 23:39:20 +0600

    server making is done

--- index.js
+++ index.js
@@ -16,11 +16,6 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// const admin = require("firebase-admin");
-// const serviceAccount = require("./prime-books-auth-firebase-adminsdk-qbt80-f990eeed6c.json");
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
 
 app.get("/", (req, res) => {
   res.send("server working");
@@ -30,69 +25,43 @@ client.connect((err) => {
   const serviceCollection = client
     .db(process.env.DB_NAME)
     .collection(process.env.DB_COL1);
+
   const reviewCollection = client
     .db(process.env.DB_NAME)
     .collection(process.env.DB_COL2);
+
   const adminCollection = client
     .db(process.env.DB_NAME)
     .collection(process.env.DB_COL3);
+    
   const appointmentCollection = client
     .db(process.env.DB_NAME)
     .collection(process.env.DB_COL4);
 
-  //  ALL BOOKS SECTION START HERE
+  // SERVICES SECTION START HERE
   // read all services
   app.get("/services", (req, res) => {
-    serviceCollection.find({})
-    .toArray((err, documents) => {
+    serviceCollection.find({}).toArray((err, documents) => {
       res.send(documents);
     });
   });
-  // read specific book by id
-  app.post("/service/:id", (req, res) => {
-    const email = req.body.email;
-    console.log(email);
-    adminCollection
-    .find({ email: email })
-    .toArray((err, documents) => {
-      if(documents.length){
-        res.send({_id: "admin"});
-      }else{
-        serviceCollection
-      .find({ _id: ObjectId(req.params.id) })
-      .toArray((err, documents) => {
-        console.log('come in')
-        res.send(documents[0]);
+  // read specific services by id
+  app.get("/service/:id", (req, res) => {
+   
+        serviceCollection.find({ _id: ObjectId(req.params.id) })
+        .toArray((err, documents) => {
+          console.log("come in");
+            res.send(documents[0]);
+          });
       });
-      }
-    })
-  });
-
   // insert new Service
   app.post("/addService", (req, res) => {
     const serviceInfo = req.body;
-    serviceCollection.insertOne(serviceInfo)
-    .then((result) => {
+    serviceCollection.insertOne(serviceInfo).then((result) => {
       console.log("data added successfully");
       res.send(result);
     });
   });
-
-  //   app.patch("/update/:id", (req, res) => {
-  //     collection
-  //       .updateOne(
-  //         { _id: ObjectId(req.params.id) },
-  //         {
-  //           $set: {
-  //             name: req.body.name,
-  //             age: req.body.age,
-  //             studyAt: req.body.studyAt,
-  //           },
-  //         }
-  //       )
-  //       .then((res) => console.log(res));
-  //   });
-
   //  delete a Service
   app.delete("/deleteService/:id", (req, res) => {
     serviceCollection
@@ -101,59 +70,56 @@ client.connect((err) => {
         res.send(result.deletedCount > 0);
       });
   });
-  // ALL BOOKS SECTION END HERE
+  // SERVICES SECTION END HERE
+
 
   //APPOINTMENT SECTION START HERE
   // add Appointment
   app.post("/addAppointment", (req, res) => {
     const appointmentInfo = req.body;
-    appointmentCollection.insertOne(appointmentInfo)
-    .then((result) => {
-      console.log("data added successfully");
+    appointmentCollection.insertOne(appointmentInfo).then((result) => {
+      console.log("appointment added successfully");
       res.send(result);
     });
   });
-
-  // read Appointments
+  // read Appointments email
   app.get("/allAppointment/:email", (req, res) => {
-    adminCollection
-            .find({ email: req.params.email })
-            .toArray((err, documents) => {
-              if (documents.length) {
-                res.send('admin');
-              } else {
-                appointmentCollection
-                  .find({ email: req.params.email })
-                  .toArray((err, documents) => {
-                    res.send(documents);
-                  });
-              }
-            });
-    // const userIdToken = req.headers.authorization.split(" ")[1];
-    // admin
-    //   .auth()
-    //   .verifyIdToken(userIdToken)
-    //   .then((decodedToken) => {
-    //     const decodedTokenEmail = decodedToken.email;
-    //     if (req.params.email === decodedTokenEmail) {
-    //       adminsCollection
-    //         .find({ email: req.params.email })
-    //         .toArray((err, documents) => {
-    //           if (documents.length) {
-    //             res.send('admin');
-    //           } else {
-    //             appointmentsCollection
-    //               .find({ email: req.params.email })
-    //               .toArray((err, documents) => {
-    //                 res.send(documents);
-    //               });
-    //           }
-    //         });
-    //     }
-    //   })
-    //   .catch((error) => {});
+    appointmentCollection
+      .find({ email: req.params.email })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
   });
-  // ORDER SECTION END HERE
+  // read all Appointments
+  app.get("/allAppointment", (req, res) => {
+    appointmentCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+  // read Appointment by id
+  app.get("/appointment/:id", (req, res) => {
+    appointmentCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+  // update a appointment status
+  app.patch("/updateStatus/:id", (req, res) => {
+    console.log(req.params.id, req.body.status);
+    appointmentCollection
+      .updateOne(
+        { _id: ObjectId(req.params.id) },
+        {
+          $set: {serStatus: req.body.status}
+        }
+      )
+      .then((result) => {
+        res.send(result.modifiedCount > 0)
+      });
+  });
+  // APPOINTMENT SECTION END HERE
+
 
   //REVIEW SECTION START HERE
   app.get("/reviews", (req, res) => {
@@ -165,22 +131,36 @@ client.connect((err) => {
   app.post("/addReview", (req, res) => {
     const serviceReview = req.body;
     console.log(serviceReview);
-    reviewCollection.insertOne(serviceReview)
-    .then((result) => {
+    reviewCollection.insertOne(serviceReview).then((result) => {
       console.log("data added successfully");
       res.send(result);
     });
   });
+  //REVIEW SECTION END HERE
 
 
+   //ADMIN SECTION START HERE
+   //add admin
   app.post("/addAdmin", (req, res) => {
     const adminEmail = req.body;
     console.log(adminEmail);
     adminCollection.insertOne(adminEmail)
     .then((result) => {
       console.log("data added successfully");
-      res.send(result.insertedCount>0);
+      res.send(result.insertedCount > 0);
     });
   });
+
+//Check is admin or not
+  app.get("/isAdmin/:email", (req, res) => {
+    const email = req.params.email;
+    console.log(email);
+    adminCollection.find({ email: email })
+    .toArray((err, documents) => {
+      res.send(documents.length > 0) ;
+      });
+  });
+   //ADMIN SECTION END HERE
+
 });
 app.listen(process.env.PORT || 8080);
